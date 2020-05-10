340a05ba8db70d43dace6c28f27dc10228e70d67
Harshita1503 <55319937+Harshita1503@users.noreply.github.com>
2020-05-10 14:58:12 +0530

    Update may 9 perfectsquare.py

--- may 9 perfectsquare.py
+++ may 9 perfectsquare.py
@@ -1,11 +1,10 @@
 '''Check whether a perfect square or not'''
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        for i in range(1, num + 1):
-            sq = i * i
-            if sq == num:
-                print(f"{i} squared is exactly {num}")
-                break
-            if sq  > num:
-                print(f"{num} is not a perfect square")
-                break
+        a=num**(0.5)
+        b=a*10
+        c=b%10
+        if(c==0):
+            return True
+        else:
+            return False
